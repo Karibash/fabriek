4ab9dbd4efac6950a239640cfdccac7df4965f0c
Karibash <y.karibayashi@gmail.com>
2022-04-20 21:32:08 +0900

    Add fabriek function

---
+++ packages/core/src/fabriek.ts
@@ -0,0 +1,32 @@
+import { build, BuilderOutput, BuilderRecord } from './build';
+
+export type Fabriek<T extends BuilderRecord> = {
+  [Key in keyof T]: (defaults?: Partial<BuilderOutput<T[Key]>>) => BuilderOutput<T[Key]>;
+};
+
+export type MiddlewareContext = {
+  key: string;
+  defaults?: Record<string, unknown>;
+};
+
+export type MiddlewareNext = (context: MiddlewareContext) => Record<string, unknown>;
+
+export type Middleware = (
+  context: MiddlewareContext,
+  next: MiddlewareNext,
+) => Record<string, unknown>;
+
+export const fabriek = <T extends BuilderRecord>(builders: T, middlewares?: Middleware[]): Fabriek<T> => {
+  const consumer = (next: MiddlewareNext, index: number = middlewares?.length ?? 0): MiddlewareNext => {
+    const middleware = middlewares?.[index - 1];
+    if (!middleware) return context => next(context);
+    return consumer(context => middleware(context, next), index - 1);
+  };
+
+  return Object.entries(builders).reduce<Record<string, unknown>>((previous, [key, value]) => {
+    previous[key] = (defaults?: Partial<BuilderOutput<typeof value>>) => {
+      return consumer(context => build(builders[context.key], context.defaults))({ key, defaults });
+    };
+    return previous;
+  }, {}) as Fabriek<T>;
+};
---
+++ packages/core/test/fabriek.test.ts
@@ -0,0 +1,100 @@
+import { Builder, fabriek, Middleware } from '@fabriek/core';
+
+type Mock = {
+  value: number;
+};
+
+const mock: Builder<Mock> = () => ({
+  value: 1,
+});
+
+describe('@fabriek/core/fabriek', () => {
+  it('should be a function', () => {
+    expect(typeof fabriek).toBe('function');
+  });
+
+  it('should be a function', () => {
+    const factory = fabriek({ mock });
+    expect(typeof factory.mock).toBe('function');
+  });
+
+  it('should be expected return value', () => {
+    const factory = fabriek({ mock });
+    expect(factory.mock()).toEqual({ value: 1 });
+  });
+
+  it('should be able specify a default value', () => {
+    const factory = fabriek({ mock });
+    expect(factory.mock({ value: 2 })).toEqual({ value: 2 });
+  });
+
+  it('should be return same value each time for static builder', () => {
+    const factory = fabriek({ mock });
+    expect(factory.mock()).toEqual(factory.mock());
+  });
+
+  it('should be return different value each time for dynamic builder', () => {
+    const factory = fabriek({ mock: (): Mock => ({ value: Math.random() }) });
+    expect(factory.mock()).not.toEqual(factory.mock());
+  });
+
+  it('should be occurs TypeError when specify not a function', () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const factory = fabriek({ mock: 1 });
+    expect(() => factory.mock()).toThrowError(TypeError);
+  });
+
+  it('should be occurs TypeError when specify not a object', () => {
+    const factory = fabriek({ mock });
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    expect(() => factory.mock(1)).toThrowError(TypeError);
+  });
+
+  it('should be middlewares invoked in the correct order', () => {
+    const fn = jest.fn();
+
+    const middlewares: Middleware[] = [
+      (context, next) => {
+        fn(1);
+        const result = next(context);
+        fn(4);
+        return result;
+      },
+      (context, next) => {
+        fn(2);
+        const result = next(context);
+        fn(3);
+        return result;
+      },
+    ];
+
+    fabriek({ mock }, middlewares).mock();
+    expect(fn.mock.calls[0][0]).toBe(1);
+    expect(fn.mock.calls[1][0]).toBe(2);
+    expect(fn.mock.calls[2][0]).toBe(3);
+    expect(fn.mock.calls[3][0]).toBe(4);
+  });
+
+  it('should be able to rewrite the return value with middleware', () => {
+    const fn = jest.fn();
+
+    const middlewares: Middleware[] = [
+      (context, next) => {
+        const result = next(context);
+        fn(result);
+        return result;
+      },
+      (context, next) => {
+        const result = next(context);
+        fn(result);
+        return { value: 2 };
+      },
+    ];
+
+    fabriek({ mock }, middlewares).mock();
+    expect(fn.mock.calls[0][0]).toEqual({ value: 1 });
+    expect(fn.mock.calls[1][0]).toEqual({ value: 2 });
+  });
+});
