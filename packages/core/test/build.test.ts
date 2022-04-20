fddf9b72ee0e8316ddc455b9307af5f5d309e8f2
Karibash <y.karibayashi@gmail.com>
2022-04-21 00:54:25 +0900

    Make the test code mock use nested data

--- packages/core/test/build.test.ts
+++ packages/core/test/build.test.ts
@@ -2,6 +2,9 @@ import { build, Builder } from '@fabriek/core';
 
 type Mock = {
   value: number;
+  nested: {
+    value: number;
+  };
 };
 
 describe('@fabriek/core/build', () => {
@@ -10,22 +13,22 @@ describe('@fabriek/core/build', () => {
   });
 
   it('should be expected return value', () => {
-    const builder: Builder<Mock> = (): Mock => ({ value: 1 });
-    expect(build(builder)).toEqual({ value: 1 });
+    const builder: Builder<Mock> = (): Mock => ({ value: 1, nested: { value: 1 } });
+    expect(build(builder)).toEqual({ value: 1, nested: { value: 1 } });
   });
 
   it('should be able specify a default value', () => {
-    const builder: Builder<Mock> = (): Mock => ({ value: 1 });
-    expect(build(builder, { value: 2 })).toEqual({ value: 2 });
+    const builder: Builder<Mock> = (): Mock => ({ value: 1, nested: { value: 1 } });
+    expect(build(builder, { value: 2, nested: { value: 2 } })).toEqual({ value: 2, nested: { value: 2 } });
   });
 
   it('should be return same value each time for static builder', () => {
-    const builder: Builder<Mock> = (): Mock => ({ value: 1 });
+    const builder: Builder<Mock> = (): Mock => ({ value: 1, nested: { value: 1 } });
     expect(build(builder)).toEqual(build(builder));
   });
 
   it('should be return different value each time for dynamic builder', () => {
-    const builder: Builder<Mock> = (): Mock => ({ value: Math.random() });
+    const builder: Builder<Mock> = (): Mock => ({ value: Math.random(), nested: { value: Math.random() } });
     expect(build(builder)).not.toEqual(build(builder));
   });
 
@@ -36,7 +39,7 @@ describe('@fabriek/core/build', () => {
   });
 
   it('should be occurs TypeError when specify not a object', () => {
-    const builder: Builder<Mock> = (): Mock => ({ value: Math.random() });
+    const builder: Builder<Mock> = (): Mock => ({ value: 1, nested: { value: 1 } });
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-expect-error
     expect(() => build(builder, 1)).toThrowError(TypeError);
--- packages/core/test/fabriek.test.ts
+++ packages/core/test/fabriek.test.ts
@@ -2,10 +2,16 @@ import { Builder, fabriek, Middleware } from '@fabriek/core';
 
 type Mock = {
   value: number;
+  nested: {
+    value: number;
+  };
 };
 
 const mock: Builder<Mock> = () => ({
   value: 1,
+  nested: {
+    value: 1,
+  },
 });
 
 describe('@fabriek/core/fabriek', () => {
@@ -20,12 +26,12 @@ describe('@fabriek/core/fabriek', () => {
 
   it('should be expected return value', () => {
     const factory = fabriek({ mock });
-    expect(factory.mock()).toEqual({ value: 1 });
+    expect(factory.mock()).toEqual({ value: 1, nested: { value: 1 } });
   });
 
   it('should be able specify a default value', () => {
     const factory = fabriek({ mock });
-    expect(factory.mock({ value: 2 })).toEqual({ value: 2 });
+    expect(factory.mock({ value: 2, nested: { value: 2 } })).toEqual({ value: 2, nested: { value: 2 } });
   });
 
   it('should be return same value each time for static builder', () => {
@@ -34,14 +40,14 @@ describe('@fabriek/core/fabriek', () => {
   });
 
   it('should be return different value each time for dynamic builder', () => {
-    const factory = fabriek({ mock: (): Mock => ({ value: Math.random() }) });
+    const factory = fabriek({ mock: (): Mock => ({ value: Math.random(), nested: { value: Math.random() } }) });
     expect(factory.mock()).not.toEqual(factory.mock());
   });
 
   it('should be occurs TypeError when specify not a function', () => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-expect-error
-    const factory = fabriek({ mock: 1 });
+    const factory = fabriek({ mock: 1, nested: { value: 1 } });
     expect(() => factory.mock()).toThrowError(TypeError);
   });
 
@@ -89,12 +95,12 @@ describe('@fabriek/core/fabriek', () => {
       (context, next) => {
         const result = next(context);
         fn(result);
-        return { value: 2 };
+        return { value: 2, nested: { value: 2 } };
       },
     ];
 
     fabriek({ mock }, middlewares).mock();
-    expect(fn.mock.calls[0][0]).toEqual({ value: 1 });
-    expect(fn.mock.calls[1][0]).toEqual({ value: 2 });
+    expect(fn.mock.calls[0][0]).toEqual({ value: 1, nested: { value: 1 } });
+    expect(fn.mock.calls[1][0]).toEqual({ value: 2, nested: { value: 2 } });
   });
 });
